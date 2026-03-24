bf18a6ab17e02225af78540cd8a951afa16ecf40
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2026-03-24 22:52:20 +0100

    feat(workers): integrate with bellatrix sync (#30)

--- crates/bellatrix-worker/src/lib.rs
+++ crates/bellatrix-worker/src/lib.rs
@@ -1,9 +1,48 @@
 // Copyright 2026 Dotanuki Labs
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+use bellatrix_core::{Bellatrix, GithubClientConfig};
 use worker::*;
 
 #[event(scheduled)]
-async fn scheduled(_req: ScheduledEvent, _env: Env, _ctx: ScheduleContext) {
-    console_log!("Scheduling task finished");
+async fn scheduled(_: ScheduledEvent, env: Env, _: ScheduleContext) {
+    let bellatrix = create_bellatrix(env).expect("failed to create bellatrix");
+    console_log!("scheduled task started");
+
+    console_log!("updating available forks");
+    match bellatrix.sync_all().await {
+        Ok(sync_outcomes) => {
+            if sync_outcomes.is_empty() {
+                console_log!("✓ no sync required");
+                console_log!("done");
+                return;
+            }
+
+            for fork in sync_outcomes {
+                console_log!(
+                    "synchronized {} ({} {} commits)",
+                    &fork.synchronized,
+                    fork.merge_type,
+                    fork.commits
+                )
+            }
+
+            console_log!("done");
+        },
+        Err(incoming) => {
+            console_error!("{}", incoming);
+        },
+    };
+}
+
+fn create_bellatrix(env: Env) -> anyhow::Result<Bellatrix> {
+    let github_api_url = env
+        .var("GITHUB_API_URL")
+        .map_or("https://api.github.com".to_string(), |url| url.to_string());
+
+    let github_token = env.var("GITHUB_TOKEN")?.to_string();
+    let github_client_config = GithubClientConfig::new(github_api_url, github_token);
+    let github_client = bellatrix_core::github::GithubClient::try_from(github_client_config)?;
+    let bellatrix = Bellatrix::new(github_client);
+    Ok(bellatrix)
 }
