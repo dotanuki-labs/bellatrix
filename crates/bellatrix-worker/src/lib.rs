23019b0e508fc26b8adb7daccb0bf10b10fb95de
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2026-03-25 23:49:59 +0100

    feat(workers): fix access to secret store (#32)

--- crates/bellatrix-worker/src/lib.rs
+++ crates/bellatrix-worker/src/lib.rs
@@ -6,7 +6,7 @@ use worker::*;
 
 #[event(scheduled)]
 async fn scheduled(_: ScheduledEvent, env: Env, _: ScheduleContext) {
-    let bellatrix = create_bellatrix(env).expect("failed to create bellatrix");
+    let bellatrix = create_bellatrix(env).await.expect("failed to create bellatrix");
     console_log!("scheduled task started");
 
     console_log!("updating available forks");
@@ -35,12 +35,16 @@ async fn scheduled(_: ScheduledEvent, env: Env, _: ScheduleContext) {
     };
 }
 
-fn create_bellatrix(env: Env) -> anyhow::Result<Bellatrix> {
+async fn create_bellatrix(env: Env) -> anyhow::Result<Bellatrix> {
     let github_api_url = env
         .var("GITHUB_API_URL")
         .map_or("https://api.github.com".to_string(), |url| url.to_string());
 
-    let github_token = env.var("GITHUB_TOKEN")?.to_string();
+    let github_token = env
+        .secret_store("GITHUB_TOKEN")?
+        .get()
+        .await?
+        .expect("invalid GITHUB_TOKEN");
     let github_client_config = GithubClientConfig::new(github_api_url, github_token);
     let github_client = bellatrix_core::github::GithubClient::try_from(github_client_config)?;
     let bellatrix = Bellatrix::new(github_client);
